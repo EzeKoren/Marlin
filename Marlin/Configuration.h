722b7f8a176eb29f37a31ad8cb8bb0af854997b9
Ezequiel Korenblum <ezequiel@losko.com.ar>
2023-01-22 15:59:52 -0300

    Homing center feedrate fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1501,7 +1501,7 @@
 #define PROBING_MARGIN 15
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_FEEDRATE (133*60)
+#define XY_PROBE_FEEDRATE (50*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_FEEDRATE_FAST (4*60)
