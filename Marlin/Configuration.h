196df169096fc57ebea23dbc2869bfe0b61b41ed
Ezequiel Korenblum <ezequiel@losko.com.ar>
2023-09-05 13:44:36 -0300

    Moving z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1517,11 +1517,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -38, -19, -0.1 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -16, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 18
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (50*60)
